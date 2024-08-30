8a0d6809c8a5ab3387a78fc58ff17eea17ac9f18
Harry Metske <harry.metske@gmail.com>
2024-08-30 13:54:53 +0200

    switch underlying matrix library from gomatrix to mautrix-go include a few dependency bumps and switch to golang 1.23

--- go.mod
+++ go.mod
@@ -3,14 +3,16 @@
 
 module github.com/sebhoss/matrix-alertmanager-receiver
 
-go 1.21
+go 1.23
+
+toolchain go1.23.0
 
 require (
-	github.com/matrix-org/gomatrix v0.0.0-20220926102614-ceba4d9f7530
 	github.com/prometheus/alertmanager v0.27.0
 	github.com/prometheus/client_golang v1.20.2
 	github.com/prometheus/common v0.57.0
 	github.com/stretchr/testify v1.9.0
+	maunium.net/go/mautrix v0.20.0
 	sigs.k8s.io/yaml v1.4.0
 )
 
@@ -19,17 +21,28 @@ require (
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-kit/log v0.2.1 // indirect
-	github.com/go-logfmt/logfmt v0.5.1 // indirect
+	github.com/go-logfmt/logfmt v0.6.0 // indirect
 	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/kr/text v0.2.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/client_model v0.6.1 // indirect
 	github.com/prometheus/procfs v0.15.1 // indirect
+	github.com/rs/zerolog v1.33.0 // indirect
 	github.com/shurcooL/httpfs v0.0.0-20230704072500-f1e31cf0ba5c // indirect
 	github.com/shurcooL/vfsgen v0.0.0-20230704071429-0000e147ea92 // indirect
-	golang.org/x/sys v0.22.0 // indirect
-	golang.org/x/text v0.16.0 // indirect
+	github.com/tidwall/gjson v1.17.3 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.1 // indirect
+	github.com/tidwall/sjson v1.2.5 // indirect
+	go.mau.fi/util v0.7.0 // indirect
+	golang.org/x/crypto v0.26.0 // indirect
+	golang.org/x/exp v0.0.0-20240823005443-9b4947da3948 // indirect
+	golang.org/x/net v0.28.0 // indirect
+	golang.org/x/sys v0.24.0 // indirect
+	golang.org/x/text v0.17.0 // indirect
 	google.golang.org/protobuf v1.34.2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/matrix/client.go
+++ internal/matrix/client.go
@@ -7,24 +7,20 @@ package matrix
 
 import (
 	"context"
-	"github.com/matrix-org/gomatrix"
+	"fmt"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/sebhoss/matrix-alertmanager-receiver/internal/config"
+	"html"
 	"log/slog"
+	"maunium.net/go/mautrix"
+	"maunium.net/go/mautrix/event"
+	"maunium.net/go/mautrix/id"
 	"os"
-	"slices"
+	"regexp"
 )
 
 var (
-	joinRoomSuccessTotal = promauto.NewCounterVec(prometheus.CounterOpts{
-		Name: "matrix_alertmanager_receiver_join_room_success_total",
-		Help: "The total number of successful join room operations",
-	}, []string{"room"})
-	joinRoomFailureTotal = promauto.NewCounterVec(prometheus.CounterOpts{
-		Name: "matrix_alertmanager_receiver_join_room_failure_total",
-		Help: "The total number of failed join room operations",
-	}, []string{"room"})
 	sendSuccessTotal = promauto.NewCounter(prometheus.CounterOpts{
 		Name: "matrix_alertmanager_receiver_send_success_total",
 		Help: "The total number of successful send operations",
@@ -37,64 +33,49 @@ var (
 
 type SendingFunc func(htmlText string, roomID string)
 
-var joinedRoomIDs []string
+// An HTMLMessage is the contents of a Matrix HTML formated message event.
+type HTMLMessage struct {
+	Body          string `json:"body"`
+	MsgType       string `json:"msgtype"`
+	Format        string `json:"format"`
+	FormattedBody string `json:"formatted_body"`
+}
+
+var htmlRegex = regexp.MustCompile("<[^<]+?>")
+
+// GetHTMLMessage returns an HTMLMessage with the body set to a stripped version of the provided HTML, in addition
+// to the provided HTML.
+func GetHTMLMessage(msgtype, htmlText string) HTMLMessage {
+	return HTMLMessage{
+		Body:          html.UnescapeString(htmlRegex.ReplaceAllLiteralString(htmlText, "")),
+		MsgType:       msgtype,
+		Format:        "org.matrix.custom.html",
+		FormattedBody: htmlText,
+	}
+}
 
 func CreatingSendingFunc(ctx context.Context, configuration config.Matrix) SendingFunc {
 	matrixClient := createMatrixClient(ctx, configuration)
-	fetchJoinedRooms(ctx, matrixClient)
-
 	return func(htmlText string, room string) {
-		msg := gomatrix.GetHTMLMessage("m.text", htmlText)
-		mappedRoom := room
-		if mapped, ok := configuration.RoomMapping[room]; ok {
-			mappedRoom = mapped
-		}
-		err := joinRoom(ctx, matrixClient, mappedRoom)
-		if err != nil {
-			joinRoomFailureTotal.WithLabelValues(mappedRoom).Inc()
-			slog.ErrorContext(ctx, "Failed to join room", slog.Any("error", err))
+		if respSendEvent, err := matrixClient.SendMessageEvent(ctx, id.RoomID(room), event.NewEventType("m.room.message"), GetHTMLMessage("m.text", htmlText)); err != nil {
+			sendFailureTotal.Inc()
+			slog.ErrorContext(ctx, "Could not send message to Matrix homeserver", slog.Any("error", err))
 		} else {
-			joinRoomSuccessTotal.WithLabelValues(mappedRoom).Inc()
-			_, err = matrixClient.SendMessageEvent(mappedRoom, "m.room.message", msg)
-			if err != nil {
-				sendFailureTotal.Inc()
-				slog.ErrorContext(ctx, "Could not send message to Matrix homeserver", slog.Any("error", err))
-			} else {
-				sendSuccessTotal.Inc()
-				slog.DebugContext(ctx, "Message sent to Matrix homeserver")
-			}
+			sendSuccessTotal.Inc()
+			slog.DebugContext(ctx, fmt.Sprintf("Message %s sent to Matrix homeserver", respSendEvent.EventID))
 		}
 	}
 }
 
-func createMatrixClient(ctx context.Context, configuration config.Matrix) *gomatrix.Client {
+func createMatrixClient(ctx context.Context, configuration config.Matrix) *mautrix.Client {
+	var err error
+	var matrixClient *mautrix.Client
 	slog.DebugContext(ctx, "Creating Matrix client", slog.Any("configuration", configuration.LogValue()))
-	matrixClient, err := gomatrix.NewClient(configuration.HomeServerURL, configuration.UserID, configuration.AccessToken)
-	if err != nil {
-		slog.ErrorContext(ctx, "Could not log in to Matrix homeserver", slog.Any("error", err))
+	if matrixClient, err = mautrix.NewClient(configuration.HomeServerURL, id.UserID(configuration.UserID), configuration.AccessToken); err != nil {
+		slog.ErrorContext(ctx, "Failed to create matrix client", slog.Any("error", err))
 		os.Exit(1)
+	} else {
+		slog.DebugContext(ctx, "Created Matrix client")
 	}
-	slog.DebugContext(ctx, "Created Matrix client")
 	return matrixClient
-}
-
-func fetchJoinedRooms(ctx context.Context, client *gomatrix.Client) {
-	joinedRooms, err := client.JoinedRooms()
-	if err != nil {
-		slog.ErrorContext(ctx, "Could not fetch Matrix rooms", slog.Any("error", err))
-		os.Exit(1)
-	}
-	joinedRoomIDs = append(joinedRoomIDs, joinedRooms.JoinedRooms...)
-}
-
-func joinRoom(ctx context.Context, client *gomatrix.Client, roomToJoin string) error {
-	if !slices.Contains(joinedRoomIDs, roomToJoin) {
-		slog.DebugContext(ctx, "Joining room", slog.String("room", roomToJoin))
-		_, err := client.JoinRoom(roomToJoin, "", nil)
-		if err != nil {
-			return err
-		}
-		joinedRoomIDs = append(joinedRoomIDs, roomToJoin)
-	}
-	return nil
 }
